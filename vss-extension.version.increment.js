69b720c8798538a589ecce15b122aca2fd5ba696
Rhys Koedijk <rhys@koedijk.co.nz>
2024-11-28 12:48:12 +1300

    When publishing the localhost package, always increment  the task patch version to a higher number than previous package

--- vss-extension.version.increment.js
+++ vss-extension.version.increment.js
@@ -5,18 +5,24 @@ function incrementPatchVersion(vssExtensionJsonPath, taskJsonPath) {
   const vssExtensionJson = JSON.parse(fs.readFileSync(vssExtensionJsonPath));
   const taskJson = JSON.parse(fs.readFileSync(taskJsonPath));
   const version = taskJson.version || { Major: 0, Minor: 0, Patch: 0 };
-  version.Patch += 1;
+
+  // Increment patch version to a revision number based on the current date
+  version.Patch = new Number(
+    new Date()
+      .toISOString()
+      .replace(/[^0-9]/g, '')
+      .slice(3, 12), // YMMDDHHmm
+  );
+
+  // Update task.json
   if (taskJson) {
     taskJson.version = version;
     fs.writeFileSync(taskJsonPath, JSON.stringify(taskJson, null, 2));
   }
+
+  // Update vss-extension.json
   if (vssExtensionJson) {
-    const revisionTimestamp = new Date()
-      .toISOString()
-      .replace(/[^0-9]/g, '')
-      .slice(3, 12); // YMMDDHHmm
-    vssExtensionJson.version =
-      taskJson.version.Major + '.' + taskJson.version.Minor + '.' + taskJson.version.Patch + '.' + revisionTimestamp;
+    vssExtensionJson.version = taskJson.version.Major + '.' + taskJson.version.Minor + '.' + taskJson.version.Patch;
     fs.writeFileSync(vssExtensionJsonPath, JSON.stringify(vssExtensionJson, null, 2));
   }
 }
