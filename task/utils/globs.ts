51d49f6d57d9219b43d1e9dfe39712a8a6645fcd
Rhys Koedijk <rhys@koedijk.co.nz>
2025-01-05 23:36:45 +1300

    Improve path glob resolution

--- task/utils/globs.ts
+++ task/utils/globs.ts
@@ -29,8 +29,27 @@ export function resolvePathGlobs(paths: string | string[], cwd?: string): string
  * @returns The files matching the path globs
  */
 export function getFilesMatchingPathGlobs(paths: string | string[], cwd?: string): string[] {
-  const resolvedPaths = resolvePathGlobs(paths, cwd);
-  return resolvedPaths
-    .flatMap((p) => (fs.lstatSync(p).isDirectory() ? getFilesMatchingPathGlobs(path.join(p, '**', '*.*')) : [p]))
+  return resolvePathGlobs(paths, cwd)
+    .flatMap((p) => getFilesInDirectoryRecursive(fs.lstatSync(p).isDirectory() ? p : path.dirname(p)))
     .distinct();
+}
+
+/**
+ * Recursively read a directory
+ * @param directory The directory to read
+ * @returns The files in the directory
+ */
+function getFilesInDirectoryRecursive(directory: string): string[] {
+  const files = fs.readdirSync(directory);
+  const resolvedFiles = [];
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    if (fs.lstatSync(filePath as string).isDirectory()) {
+      resolvedFiles.push(...getFilesInDirectoryRecursive(filePath));
+    } else if (fs.lstatSync(filePath as string).isFile()) {
+      resolvedFiles.push(filePath);
+    }
+  }
+  console.log('DIR FILES', directory, resolvedFiles);
+  return resolvedFiles;
 }
