db41e2fe437d77efe6cc09a82ec35a507a34029f
Rhys Koedijk <rhys@koedijk.co.nz>
2025-04-06 23:52:35 +1200

    Fix unit tests

--- shared/spdx/models/2.3/document.test.ts
+++ shared/spdx/models/2.3/document.test.ts
@@ -2,24 +2,24 @@ import * as fs from 'fs';
 
 import { expect } from '@jest/globals';
 
-import { getPackageAncestorPaths, IDocument } from './document';
+import { getPackageAncestorDependencyPaths, IDocument } from './document';
 
-describe('getPackageAncestorPaths', () => {
+describe('getPackageAncestorDependencyPaths', () => {
   const document = JSON.parse(fs.readFileSync('tests/spdx/manifest.spdx.json', 'utf-8')) as IDocument;
 
   it('should return an empty array if the package is not found', () => {
-    const result = getPackageAncestorPaths(document, 'SPDXRef-NonExistentPackage');
+    const result = getPackageAncestorDependencyPaths(document, 'SPDXRef-NonExistentPackage');
     expect(result).toEqual([]);
   });
 
   it('should return an empty array if the package has no ancestors', () => {
-    const result = getPackageAncestorPaths(document, 'SPDXRef-RootPackage');
+    const result = getPackageAncestorDependencyPaths(document, 'SPDXRef-RootPackage');
     expect(result).toEqual([]);
   });
 
   it('should return the correct dependency path for a package with ancestors', () => {
     const targetPackage = document.packages.find((p) => p.name === 'Microsoft.Identity.Web')?.SPDXID || '';
-    const result = getPackageAncestorPaths(document, targetPackage);
+    const result = getPackageAncestorDependencyPaths(document, targetPackage);
     expect(result).toMatchObject([
       { dependencyPath: [{ name: 'Microsoft.Identity.Web.UI' }, { name: 'Microsoft.Identity.Web' }] },
     ]);
