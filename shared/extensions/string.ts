a32869a23ca7ca48eb45fe741803baa3ecd9dc81
Rhys Koedijk <123988+rhyskoedijk@users.noreply.github.com>
2025-08-22 13:01:25 +1200

    Truncate XLSX cells longer than 32767 (#189)
    * Truncate XLSX cells longer than 32767
    
    * Cleanup
    
    ---------
    
    Co-authored-by: Rhys Koedijk <rhys@koedijk.co.nz>

--- shared/extensions/string.ts
+++ shared/extensions/string.ts
@@ -1,6 +1,7 @@
 declare global {
   interface String {
     toPascalCase(): string;
+    truncate(maxLength: number): string;
   }
 }
 
@@ -9,6 +10,13 @@ String.prototype.toPascalCase = function (): string {
     .split(/[^a-zA-Z0-9]/)
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join('');
+};
+
+String.prototype.truncate = function (maxLength: number): string {
+  if (this.length <= maxLength) {
+    return String(this);
+  }
+  return String(this).slice(0, maxLength - 3) + '...';
 };
 
 export {};
